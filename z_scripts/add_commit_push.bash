abab6aa95f928be8bcecce7640b5b54aad9c76ca
KoreaHaos <koreahaos@gmail.com>
2017-02-15 15:50:28 +0000

    Scripted Commit!

--- z_scripts/add_commit_push.bash
+++ z_scripts/add_commit_push.bash
@@ -1,16 +1,10 @@
-
 function init(){
-    printf "$PWD\n";
     local DIR;
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-    printf "%s\n" "$DIR"
     local DIR_TO_ACT_ON;
     DIR_TO_ACT_ON=${DIR%*/z_scripts}
-    printf "%s\n" "$DIR_TO_ACT_ON"
     cd $DIR_TO_ACT_ON;
-    printf "$PWD\n";
     add_commit_push;
-    #cut -d'/' -f1 $DIR
 }
 
 function add_commit_push(){
